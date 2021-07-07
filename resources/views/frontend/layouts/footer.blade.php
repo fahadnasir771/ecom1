6b29fc4a8f7ba47774e27281cde0c3a887a9f998
Fahad <fahad.nasir771@gmail.com>
2021-07-07 23:55:20 +0500

    Commit 6

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -78,7 +78,7 @@
 						</div>
 						<div class="col-lg-6 col-12">
 							<div class="right">
-								<img src="{{asset('backend/img/payments.png')}}" alt="#">
+								{{-- <img src="{{asset('backend/img/payments.png')}}" alt="#"> --}}
 							</div>
 						</div>
 					</div>
